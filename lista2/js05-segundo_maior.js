b3d5dc51e70d4adae117ee1851782b11ae7632e9
Seu Nome <voce@exemplo.com>
2022-08-12 11:59:53 -0300

    "Otimização do exercício 2 da lista 2"

--- lista2/js05-segundo_maior.js
+++ lista2/js05-segundo_maior.js
@@ -10,24 +10,30 @@
  * Não usar sort
  * Não usar funções built-ins da linguagem
  */
-let lista = [1, 4, 7 , 8 , 33, 8, 20]
+ var lista = [42, 44, 57, 44, 77, 39, 51, 46, 32, 93,
+    40, 32, 98, 97, 50, 27, 70, 12, 46, 90, 52, 40, 26, 91, 5, 55, 4, 61, 26, 88,
+    51, 53, 47, 94, 49, 25, 60, 77, 98, 20, 17, 81, 79, 32, 48, 98, 31, 100, 43, 78];
 let maior = 0
-let indice = 0;
+let seg_maior = 0
 
-for (var i in lista){
+for (var i in lista + 1){
     if (lista[i] > maior){
+        seg_maior = maior
         maior = lista[i]
-        indice = i
     }
-}
-lista.splice(indice, 1)
-console.log(lista)
-seg_maior = 0
-indice = 0;
-for (var i in lista){
-    if (lista[i] > seg_maior){
+    if (lista[i] > seg_maior && lista[i] < maior){
         seg_maior = lista[i]
-        indice = i
     }
 }
+console.log(`O maior é ${maior}`)
+// lista.splice(indice, 1)
+// console.log(lista)
+// seg_maior = 0
+// indice = 0;
+// for (var i in lista){
+//     if (lista[i] > seg_maior){
+//         seg_maior = lista[i]
+//         indice = i
+//     }
+// }
 console.log(`O segundo maior é ${seg_maior}`)
